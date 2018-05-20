13dc61bdddc971643e8d6b85ee8f6dfafe97db54
kalenikm <kalenikmisha@gmail.com>
2018-05-20 15:42:06 +0300

    Logic and Tests were added

---
+++ Logic.Tests/RandomArrayGeneratorTests.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Linq;
+using NUnit.Framework;
+
+namespace Logic.Tests
+{
+    [TestFixture]
+    public class RandomArrayGeneratorTests
+    {
+        [TestCase(1234)]
+        [TestCase(3)]
+        [TestCase(357235)]
+        [TestCase(121)]
+        [TestCase(12112354)]
+        public void GetRandomArray_FirstMethod_NormalCount(int count)
+        {
+            var result = RandomArrayGenerator.GetRandomArray1(count);
+            var sum = result.Sum();
+
+            Assert.IsFalse(sum > 1 || sum < 1);
+        }
+
+        [TestCase(3575)]
+        [TestCase(121)]
+        [TestCase(12112)]
+        public void GetRandomArray_FirstMethod_CheckAllElements(int count)
+        {
+            var result = RandomArrayGenerator.GetRandomArray1(count);
+
+            foreach (var number in result)
+            {
+                Assert.IsTrue(number > 0 || number < 1);
+            }
+        }
+
+        [TestCase(0)]
+        [TestCase(-100)]
+        public void GetRandomArray_FirstMethod_InvalidCount_ArgumentException(int count)
+        {
+            Assert.Catch<ArgumentException>(() => RandomArrayGenerator.GetRandomArray1(count));
+        }
+
+        [TestCase(1234)]
+        [TestCase(124234)]
+        [TestCase(3)]
+        [TestCase(35732235)]
+        [TestCase(1231)]
+        public void GetRandomArray_SecondMethod_NormalCount(int count)
+        {
+            var result = RandomArrayGenerator.GetRandomArray2(count);
+            var sum = result.Sum();
+
+            Assert.IsFalse(sum > 1 || sum < 1);
+        }
+
+        [TestCase(3575)]
+        [TestCase(121)]
+        [TestCase(12112)]
+        public void GetRandomArray_SecondMethod_CheckAllElements(int count)
+        {
+            var result = RandomArrayGenerator.GetRandomArray2(count);
+
+            foreach (var number in result)
+            {
+                Assert.IsTrue(number > 0 || number < 1);
+            }
+        }
+
+        [TestCase(0)]
+        [TestCase(-100)]
+        public void GetRandomArray_SecondMethod_InvalidCount_ArgumentException(int count)
+        {
+            Assert.Catch<ArgumentException>(() => RandomArrayGenerator.GetRandomArray2(count));
+        }
+    }
+}
---
+++ Logic/RandomArrayGenerator.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace Logic
+{
+    public static class RandomArrayGenerator
+    {
+        /// <summary>
+        /// Returns an array with random double values. Sum of this elements is equals 1.
+        /// </summary>
+        /// <param name="count">Count of elements in array.</param>
+        /// <returns>Array with random double values.</returns>
+        public static double[] GetRandomArray1(int count)
+        {
+            if (count <= 0)
+            {
+                throw new ArgumentException("Count can't be less or equals zero.");
+            }
+
+            Random rand = new Random((int)DateTime.Now.Ticks);
+
+            double[] array = new double[count];
+            double sum = 0;
+
+            for (int i = 0; i < count - 1; i++)
+            {
+                array[i] = rand.NextDouble() % ((1 - sum) * 0.3);
+                while (array[i] <= 0)
+                {
+                    array[i] = rand.NextDouble() % ((1 - sum) * 0.3);
+                }
+                sum += array[i];
+            }
+            array[count - 1] = 1 - sum;
+
+            return array;
+        }
+
+        /// <summary>
+        /// Returns an array with random double values. Sum of this elements is equals 1.
+        /// </summary>
+        /// <param name="count">Count of elements in array.</param>
+        /// <returns>Array with random double values.</returns>
+        public static double[] GetRandomArray2(int count)
+        {
+            if (count <= 0)
+            {
+                throw new ArgumentException("Count can't be less or equals zero.");
+            }
+
+            Random rand = new Random((int)DateTime.Now.Ticks);
+
+            double[] array = new double[count];
+            long divider = 0;
+
+            for (int i = 0; i < count; i++)
+            {
+                array[i] = rand.Next(1, Int32.MaxValue);
+                divider += (long)array[i];
+            }
+
+            double sum = 0;
+
+            for (int i = 0; i < count - 1; i++)
+            {
+                array[i] = array[i] / divider;
+                sum += array[i];
+            }
+            array[count - 1] = 1 - sum;
+
+            return array;
+        }
+    }
+}
